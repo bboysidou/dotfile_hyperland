edf1f1e86dcf5203d63601a737cb73d2542d67f7
bboysidou <bedrani.sidali.94@gmail.com>
2024-08-05 19:54:28 +0100

    feat: correcting the hypr master syntax

--- hypr/scripts/xdg.sh
+++ hypr/scripts/xdg.sh
@@ -1,9 +1,9 @@
 sleep 1
 
 # kill all possible running xdg-desktop-portals
-killall xdg-desktop-portal-hyprland
+killall -e xdg-desktop-portal-hyprland
+killall -e xdg-desktop-portal-wlr
 killall xdg-desktop-portal
-sleep 1
 
 # start xdg-desktop-portal-hyprland
 /usr/lib/xdg-desktop-portal-hyprland &
@@ -11,4 +11,3 @@ sleep 2
 
 # start xdg-desktop-portal
 /usr/lib/xdg-desktop-portal &
-sleep 1
