d18ff555671db8955965776fda0c3c673d265d2b
bboysidou <bedrani.sidali.94@gmail.com>
2025-02-03 16:18:48 +0100

    feat: make split and new window navigate to the current pane path

--- custom_scripts/ssh_connection.sh
+++ custom_scripts/ssh_connection.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-server_names=`echo "sidouxp3 sidou evo_production evo_gestion femispace" | tr ' ' '\n'`
-server_ips=("sidouxp3" "sidou" "evo_production" "evo_gestion" "femispace")
+server_names=`echo "sidouxp3 sidou fivation" | tr ' ' '\n'`
+server_ips=("sidouxp3" "sidou" "fivation")
 
 selected=`echo "$server_names"| cat -n | fzf --reverse --border "rounded" --border-label "SSH CONNECTION" --with-nth 2.. | awk '{print $1}'`
 
