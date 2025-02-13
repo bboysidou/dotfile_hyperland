146bb56ffe531b9bec7b7b0cef33f56da15dabba
bboysidou <bedrani.sidali.94@gmail.com>
2025-02-13 21:18:25 +0100

    feat: add dap debugger

--- nvim/lua/sidouxp3/plugins/lsp/mason.lua
+++ nvim/lua/sidouxp3/plugins/lsp/mason.lua
@@ -57,6 +57,7 @@ return {
         "stylua", -- lua formatter
         "isort", -- python formatter
         "black", -- python formatter
+        "clang-format", -- c formatter
         "pylint", -- python linter
         "eslint_d", -- js linter
         "sql-formatter", -- sql linter
