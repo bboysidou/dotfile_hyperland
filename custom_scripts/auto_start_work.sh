616e8ed474b2bf97d47ec9496723c4fe78065854
bboysidou <bedrani.sidali.94@gmail.com>
2025-03-27 19:14:39 +0100

    feat: change nvim colorschem | add security features for arch linux

--- custom_scripts/auto_start_work.sh
+++ custom_scripts/auto_start_work.sh
@@ -24,6 +24,3 @@ firefox --new-window &
 sleep 1 
 hyprctl dispatch workspace 2
 kitty --start-as=fullscreen sh $HOME/.config/custom_scripts/tmux_resurect_session.sh
-
-sleep 1 
-notify-send "Setup Started" "Happy Coding!"
--- nvim/lua/sidouxp3/plugins/colorscheme.lua
+++ nvim/lua/sidouxp3/plugins/colorscheme.lua
@@ -1,38 +1,83 @@
 return {
-  "folke/tokyonight.nvim",
+  "rockyzhang24/arctic.nvim",
+  dependencies = { "rktjmp/lush.nvim" },
+  name = "arctic",
+  branch = "main",
   priority = 1000,
   config = function()
-    local bg = "#000000"
-    local bg_dark = "#000000"
-    local bg_highlight = "#011423"
-    local bg_search = "#0A64AC"
-    local bg_visual = "#275378"
-    local fg = "#CBE0F0"
-    local fg_dark = "#B4D0E9"
-    local fg_gutter = "#627E97"
-    local border = "#547998"
-    --
-    require("tokyonight").setup({
-      style = "night",
-      on_colors = function(colors)
-        colors.bg = bg
-        colors.bg_dark = bg_dark
-        colors.bg_float = bg_dark
-        colors.bg_highlight = bg_highlight
-        colors.bg_popup = bg_dark
-        -- colors.bg_search = bg_search
-        colors.bg_sidebar = bg_dark
-        --     colors.bg_statusline = bg_dark
-        --     colors.bg_visual = bg_visual
-        colors.border = border
-        -- colors.fg = fg
-        colors.fg_dark = fg_dark
-        colors.fg_float = fg
-        colors.fg_gutter = fg_gutter
-        colors.fg_sidebar = fg_dark
+    -- Apply Arctic colorscheme
+    vim.cmd("colorscheme arctic")
+
+    -- Force Netrw to use full background
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "netrw",
+      callback = function()
+        -- Set Netrw background and cursor highlight
+        vim.cmd("highlight NetrwNormal guibg=#000000")
+        vim.cmd("highlight NetrwCursorLine guibg=#111111 ctermbg=235")
+
+        -- Force Netrw to use CursorLine correctly
+        vim.cmd("setlocal winhighlight=Normal:NetrwNormal,CursorLine:NetrwCursorLine")
       end,
     })
 
-    vim.cmd("colorscheme tokyonight-night")
+    -- General UI Fixes
+    vim.cmd("highlight Normal guibg=#000000")
+    vim.cmd("highlight NormalNC guibg=#000000")
+    vim.cmd("highlight EndOfBuffer guibg=#000000")
+    vim.cmd("highlight SignColumn guibg=#000000")
+
+    -- Ensure Netrw folders are blue
+    vim.cmd("highlight NetrwDir guifg=#61afef guibg=#000000")
+
+    -- LSP completion menu black
+    vim.cmd("highlight Pmenu guibg=#000000 guifg=#ffffff")
+    vim.cmd("highlight PmenuSel guibg=#222222 guifg=#ffffff")
+    vim.cmd("highlight PmenuThumb guibg=#444444")
+    vim.cmd("highlight PmenuSbar guibg=#111111")
+
+    -- Ensure Bufferline, Telescope, and Mason also have black backgrounds
+    vim.cmd("highlight TelescopeNormal guibg=#000000")
+    vim.cmd("highlight MasonNormal guibg=#000000")
+    vim.cmd("highlight BufferLineFill guibg=#000000")
   end,
 }
+
+-- return {
+--   "folke/tokyonight.nvim",
+--   priority = 1000,
+--   config = function()
+--     local bg = "#000000"
+--     local bg_dark = "#000000"
+--     local bg_highlight = "#011423"
+--     -- local bg_search = "#0A64AC"
+--     -- local bg_visual = "#275378"
+--     local fg = "#CBE0F0"
+--     local fg_dark = "#B4D0E9"
+--     local fg_gutter = "#627E97"
+--     local border = "#547998"
+--     --
+--     require("tokyonight").setup({
+--       style = "night",
+--       on_colors = function(colors)
+--         colors.bg = bg
+--         colors.bg_dark = bg_dark
+--         colors.bg_float = bg_dark
+--         colors.bg_highlight = bg_highlight
+--         colors.bg_popup = bg_dark
+--         -- colors.bg_search = bg_search
+--         colors.bg_sidebar = bg_dark
+--         --     colors.bg_statusline = bg_dark
+--         --     colors.bg_visual = bg_visual
+--         colors.border = border
+--         -- colors.fg = fg
+--         colors.fg_dark = fg_dark
+--         colors.fg_float = fg
+--         colors.fg_gutter = fg_gutter
+--         colors.fg_sidebar = fg_dark
+--       end,
+--     })
+--
+--     vim.cmd("colorscheme tokyonight-night")
+--   end,
+-- }
