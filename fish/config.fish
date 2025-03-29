2abafa2e8628373a37724aff9ef04dcd91a94817
bboysidou <bedrani.sidali.94@gmail.com>
2025-03-29 01:30:05 +0100

    feat: new packages in postinstall script add fish and kitty

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,9 @@
+set -gx EDITOR "nvim"
+set -gx VISUAL "nvim"
+
+set -Ux SQLFLUFF_CONFIG $HOME/.config/nvim/lua/sidouxp3/plugins/lsp/.sqlfluff
+set -Ux SQL_FORMATTER_CONFIG $HOME/.config/nvim/lua/sidouxp3/plugins/lsp/.sql-formatter.json
+
 alias tmuxa="sh ~/.config/custom_scripts/tmux_add_session.sh"
 alias sysupdate="sudo pacman -Syu"
 alias sysclean="paccache -r & sudo pacman -R $(pacman -Qtdq)"
@@ -8,3 +14,7 @@ alias la="eza --icons -al"
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
