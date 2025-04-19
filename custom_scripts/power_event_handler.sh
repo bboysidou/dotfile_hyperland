a9f958d05f3ee08fd1bbf9eb2830073ef4ff8d69
bboysidou <bedrani.sidali.94@gmail.com>
2025-04-19 14:46:24 +0100

    fix: add udev rules for plugged and not plugged AC notification and sound

--- custom_scripts/power_event_handler.sh
+++ custom_scripts/power_event_handler.sh
@@ -7,27 +7,26 @@
 # cat /sys/class/power_supply/{ADAPTER}/type
 # it should display "Mains"
 
-#!/bin/bash
-
+USER_NAME=$(loginctl list-users | awk '$1 != "UID" && $1 != "0" {print $2}' | head -n 1)
+USER_ID=$(id -u "$USER_NAME")
 POWER_STATE=$(cat /sys/class/power_supply/ADP0/online)
+
+export DISPLAY=:0
+export XDG_RUNTIME_DIR="/run/user/$USER_ID"
+export DBUS_SESSION_BUS_ADDRESS="unix:path=$XDG_RUNTIME_DIR/bus"
 
 log() {
   echo "$(date): $1" >> /var/log/power-events.log
 }
 
-# Environment for GUI actions
-export DISPLAY=:0
-export XDG_RUNTIME_DIR="/run/user/1000"
-USER=$(whoami)
-
 if [[ "$POWER_STATE" -eq 1 ]]; then
   log "AC plugged in – You are now connected to AC power."
-  sudo -u $USER DISPLAY=$DISPLAY XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR notify-send "🔌 AC plugged in" "You are now connected to AC power."
-  sudo -u $USER XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR paplay /usr/share/sounds/freedesktop/stereo/power-plug.oga
+  sudo -u $USER_NAME DISPLAY=$DISPLAY XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR notify-send "🔌 AC plugged in" "You are now connected to AC power."
+  sudo -u $USER_NAME XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR paplay /usr/share/sounds/freedesktop/stereo/power-plug.oga
   brightnessctl set 100%
 else
   log "On battery – You are using battery power."
-  sudo -u $USER DISPLAY=$DISPLAY XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR notify-send "🔋 On battery" "You are now on battery power."
-  sudo -u $USER XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR paplay /usr/share/sounds/freedesktop/stereo/power-unplug.oga
+  sudo -u $USER_NAME DISPLAY=$DISPLAY XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR notify-send "🔋 On battery" "You are now on battery power."
+  sudo -u $USER_NAME XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR paplay /usr/share/sounds/freedesktop/stereo/power-unplug.oga
   brightnessctl set 50%
 fi
