05616e48da29c11638827fedecf68bfa1adb7810
bboysidou <bedrani.sidali.94@gmail.com>
2025-04-20 14:30:57 +0100

    feat: my tmux theme

--- nvim/lua/sidouxp3/plugins/lualine.lua
+++ nvim/lua/sidouxp3/plugins/lualine.lua
@@ -6,30 +6,34 @@ return {
     local lazy_status = require("lazy.status") -- to configure lazy pending updates count
 
     local colors = {
-      blue = "#65D1FF",
-      green = "#3EFFDC",
-      violet = "#FF61EF",
-      yellow = "#FFDA7B",
+      blue = "#569CD6",
+      green = "#4EC9B0",
+      violet = "#C586C0",
+      yellow = "#D7BA7D",
       red = "#FF4A4A",
-      fg = "#c3ccdc",
+      white = "#FFF",
+      black = "#000",
+      fg = "#C3CCDC",
       bg = "#112638",
-      inactive_bg = "#2c3043",
+      inactive_bg = "#2C3043",
+
+      -- green = "#3EFFDC",
     }
 
     local my_lualine_theme = {
       normal = {
         a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
-        b = { bg = colors.bg, fg = colors.fg },
+        b = { bg = colors.yellow, fg = colors.bg },
         c = { bg = colors.bg, fg = colors.fg },
       },
       insert = {
         a = { bg = colors.green, fg = colors.bg, gui = "bold" },
-        b = { bg = colors.bg, fg = colors.fg },
+        b = { bg = colors.yellow, fg = colors.white },
         c = { bg = colors.bg, fg = colors.fg },
       },
       visual = {
         a = { bg = colors.violet, fg = colors.bg, gui = "bold" },
-        b = { bg = colors.bg, fg = colors.fg },
+        b = { bg = colors.yellow, fg = colors.white },
         c = { bg = colors.bg, fg = colors.fg },
       },
       command = {
@@ -39,7 +43,7 @@ return {
       },
       replace = {
         a = { bg = colors.red, fg = colors.bg, gui = "bold" },
-        b = { bg = colors.bg, fg = colors.fg },
+        b = { bg = colors.yellow, fg = colors.white },
         c = { bg = colors.bg, fg = colors.fg },
       },
       inactive = {
@@ -52,9 +56,12 @@ return {
     -- configure lualine with modified theme
     lualine.setup({
       options = {
+        icons_enabled = true,
         theme = my_lualine_theme,
       },
       sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
         lualine_c = { { "filename", path = 3 } },
         lualine_x = {
           {
@@ -66,6 +73,8 @@ return {
           { "fileformat" },
           { "filetype" },
         },
+        lualine_y = {},
+        lualine_z = { "location" },
       },
     })
   end,
