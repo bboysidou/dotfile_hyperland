6711f9b051127306f7a2c6303300bf21657ef575
bboysidou <bedrani.sidali.94@gmail.com>
2025-05-03 14:12:03 +0100

    feat: make the ssh script automatically take the servers from ~/.ssh/config

--- custom_scripts/ssh_connection.sh
+++ custom_scripts/ssh_connection.sh
@@ -1,22 +1,17 @@
 #!/bin/sh
 
-server_names=$(echo "sidouxp3 sidou fivation sifartek" | tr ' ' '\n')
-server_ips=("sidouxp3" "sidou" "fivation" "sifartek")
-
-selected=$(echo "$server_names" | cat -n | fzf --reverse --border "rounded" --border-label "SSH CONNECTION" --with-nth 2.. | awk '{print $1}')
-
+selected=$(cat ~/.ssh/config | grep -w Host| fzf --reverse --border "rounded" --border-label "SSH CONNECTION" --with-nth 2.. | awk -F ' ' '{print $2}')
 if [ -z "$selected" ]; then
     exit 0
 fi
 
-server=${server_ips[selected-1]}
 SESSION_NAME="SSH_SERVERS"
-RAW_NAME=$(echo "$server" | tr '.' '_')
+RAW_NAME=$(echo "$selected" | tr '.' '_')
 WINDOW_NAME="ssh_${RAW_NAME}"
 
 # Create session if it doesn't exist
 if ! tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
-    tmux new-session -d -s "$SESSION_NAME" -n "$WINDOW_NAME" "ssh $server"
+    tmux new-session -d -s "$SESSION_NAME" -n "$WINDOW_NAME" "ssh $selected"
     tmux attach-session -t "$SESSION_NAME"
     exit 0
 fi
@@ -29,6 +24,6 @@ if tmux list-windows -t "$SESSION_NAME" | grep -q "^$WINDOW_NAME"; then
 fi
 
 # create a new window for the server if it doesn't exist
-tmux new-window -t "$SESSION_NAME" -n "$WINDOW_NAME" "ssh $server"
+tmux new-window -t "$SESSION_NAME" -n "$WINDOW_NAME" "ssh $selected"
 tmux select-window -t "$SESSION_NAME:$WINDOW_NAME"
 tmux attach-session -t "$SESSION_NAME"
