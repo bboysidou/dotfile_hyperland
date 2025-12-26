246977a77a5120fdb8f587c834d43f6ecfa8599d
bboysidou <bedrani.sidali.94@gmail.com>
2025-12-26 02:27:36 +0100

    feat: auto install arch with post install

---
+++ install/core/packages.sh
@@ -0,0 +1,183 @@
+linux=(
+  # LINUX HEADERS -----------------------------------------------------------
+  linux-firmware
+  linux-firmware-marvell 
+  linux-firmware-qlogic
+  linux 
+  linux-headers
+)
+
+general=(
+  # APP NEEDED TO WINDOW MANAGER WORK PROPERLLY -------------------
+  dunst 
+  rofi
+  firefox 
+  chromium
+  neovim
+  zsh
+  tmux
+  ntfs-3g
+  unzip
+  ripgrep
+  gvfs-mtp
+  net-tools
+  mtpfs
+  fastfetch
+  usbutils
+  udisks2
+  udiskie
+  acpi
+  dhcpcd
+  fzf
+  zip
+  mpv
+  pacman-contrib
+  cronie
+  brightnessctl
+  thunar
+  tumbler
+  transmission-gtk
+  nvtop
+  man-db
+  eza
+  slurp
+  grim
+  chafa
+  yazi
+  ffmpeg
+  p7zip
+  7zip
+  jq
+  poppler
+  fd
+  fzf
+  zoxide
+  imagemagick
+  unrar
+  vi
+  fish
+  kitty
+  papirus-icon-theme
+  rsync
+  obs-studio
+  evince
+  feh
+  tree
+  gcc
+  gdb
+  make
+  valgrind
+  clang
+  papirus-icon-theme
+  curl
+  # gtk-engine-murrine
+  sassc
+  speedtest-cli
+  android-tools
+  exfatprogs
+  uwsm
+  trash-cli
+
+  # FOR OCR ------------------------------------------------------- 
+  tesseract-data
+
+  # NODE NPM ------------------------------------------------------
+  nodejs 
+  npm 
+
+  # Docker --------------------------------------------------------
+  docker 
+  docker-compose 
+
+  # BLUETOOTH -----------------------------------------------------
+  bluez
+  bluez-utils
+  bluez-tools
+  blueman
+
+  # AMD CPU -------------------------------------------------------
+  amd-ucode
+  
+  # PERSONALIZATIONS --------------------------------------------------
+  noto-fonts
+  noto-fonts-cjk
+  noto-fonts-emoji
+  noto-fonts-extra
+  ttf-jetbrains-mono-nerd
+  ttf-fira-code
+  
+  # HARDWARE ACCELERATION -----------------------------------------
+  mesa-utils
+  libva-mesa-driver
+  vdpauinfo
+  # libva-vdpau-driver
+  # mesa-vdpau
+  libva-utils
+  
+  # FIREWALL
+  nftables
+  ufw
+
+  # APPLICATION CONFINEMENT (SELINUX, APPARMOR)
+  apparmor
+
+  # MOUSE MX MASTER -----------------------------------------
+  gnome-shell-extensions 
+  solaar
+)
+
+audio=(
+  ffmpeg
+  alsa-utils
+  alsa-plugins
+  alsa-lib
+  alsa-firmware
+  a52dec
+  faac
+  faad2
+  flac
+  jasper
+  lame
+  libdca
+  libdv
+  libmad
+  libmpeg2
+  libtheora
+  libvorbis
+  libxv
+  wavpack
+  x264
+  xvidcore
+  vlc
+  pipewire
+  wireplumber
+  pipewire-audio
+  pipewire-alsa
+  pipewire-pulse
+  pipewire-jack 
+  sof-firmware
+  pavucontrol
+  pamixer
+)
+
+window_manager=(
+  hyprland
+  wl-clipboard
+  waybar
+  hyprpaper
+  hyprlock
+  xdg-desktop-portal-hyprland
+  wayland 
+  wayland-protocols 
+  xorg-xwayland 
+  qt5-wayland 
+  qt6-wayland 
+  polkit-kde-agent 
+)
+
+aur=(
+  brave-bin
+  pacseek
+  trizen
+  nwg-look
+)
